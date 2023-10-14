9d5340fbceabd9f319fc6ebe0b0d75b0ff43e1a1
Gjngx <giangdang2k2@gmail.com>
2023-10-14 23:31:55 +0700

    G1

--- src/components/bodyRight.js
+++ src/components/bodyRight.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Mapbox from './mapbox'
 import MapContainer from './MapContainer'
-import AddressSearch from './AddressSearch'
 export default function BodyRight() {
   return (
     <div className='bodyRight'>
         <div className='map-container'>
             {/* <Mapbox/> */}
-            {/* <AddressSearch/> */}
             <MapContainer/>
         </div>
     </div>
--- src/components/directionMap.js
+++ src/components/directionMap.js
@@ -1,47 +1,69 @@
-import React from 'react'
-import { TextField, Button } from '@mui/material'
-import SearchIcon from '@mui/icons-material/Search';
+import React,{useState} from 'react'
+import { TextField, Button, IconButton, ButtonGroup } from '@mui/material'
 import SwapVerticalCircleIcon from '@mui/icons-material/SwapVerticalCircle';
 import './css/directionMap.css'
 
 import styled from 'styled-components';
 
 export default function DirectionMap() {
+    const [startPoint, setStartPoint] = useState('');
+    const [endPoint, setEndPoint] = useState('');
+
     const TextFieldStyled = styled(TextField)`
-    width: 80%;
+    width: 100%;
     `
+
+    const ButtonGroupStyled = styled(ButtonGroup)`
+    display: grid;
+    width: 100%;
+    margin-top: 5px;
+    `
+    const ButtonStyled = styled(Button)`
+    width: 90%;
+    `
+    const handleButtonClick = () => {
+        setStartPoint(endPoint);
+        setEndPoint(startPoint);
+      };
     
-    const ButtonSearchStyled = styled(Button)`
-    width: 40%;
-    `
-   
-  return (
-    <div>
-        <div className='search-container' >
-            <div className="direction-container">
-                    <TextFieldStyled name = 'search-info' 
-                        id="outlined-basic" 
-                        margin="dense"
-                        label="Điểm xuất phát"  
-                        size="small" ></TextFieldStyled>  
-                    <TextFieldStyled name = 'search-info' 
-                        id="outlined-basic" 
-                        margin="dense"
-                        label="Điểm kết thúc"  
-                        size="small" ></TextFieldStyled>
-                    <div className='button'>      
-                             
-                    <ButtonSearchStyled size="small" 
-                    variant="contained" 
-                    >Bắt đầu</ButtonSearchStyled>
-                    <ButtonSearchStyled size="small" 
-                    variant="contained" 
-                    ><SwapVerticalCircleIcon/></ButtonSearchStyled>
+    return (
+        <div>
+            <div className='direction-container' >
+                <div className="direction-control">
+                    <div className='direction-textfill'>
+                        <p className='header-title'>Tìm đường:</p>
+                        <TextFieldStyled name='search-info'
+                            id="outlined-basic"
+                            margin="dense"
+                            label="Điểm xuất phát"
+                            size="small"
+                            value={startPoint}
+                            onChange={(e) => setStartPoint(e.target.value)} ></TextFieldStyled>
+                        <TextFieldStyled name='search-info'
+                            id="outlined-basic"
+                            margin="dense"
+                            label="Điểm kết thúc"
+                            size="small"
+                            value={endPoint}
+                            onChange={(e) => setEndPoint(e.target.value)} ></TextFieldStyled>
                     </div>
-                    
+                    <div className='button'>
+                        <ButtonGroupStyled variant="contained" aria-label="outlined primary button group">
+                            <ButtonStyled
+                                variant="contained"
+                            >Bắt đầu</ButtonStyled>
+
+                            <IconButton aria-label="swap"
+                            onClick={handleButtonClick} >
+                                <SwapVerticalCircleIcon />
+                            </IconButton>
+                        </ButtonGroupStyled>
+                    </div>
+                </div>
+                <div className="direction-info">
+                    <p>Đường đi</p> 
+                </div>
             </div>
         </div>
-
-    </div>
-  )
+    )
 }
