5b889ca75ff3d6ebd74406e7dc5afbd730978e49
phuong226 <thanhphuongtp2002@gmail.com>
2023-10-14 18:56:51 +0700

    P

---
+++ src/components/directMap.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './css/infoListTrain.css';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import TramIcon from '@mui/icons-material/Tram';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    maxWidth: 350,
+  }));
+  const nameTrain = `Tuyến số Metro số 1`;
+
+export default function InfoTrain() {
+  
+  return (
+    // <div className='info-train'>
+    //     <ButtonStyled> info train 1 </ButtonStyled>
+    // </div>
+    <Box sx={{ flexGrow: 1, overflow: 'hidden', px: 0.5 }}>
+    <Item
+      sx={{
+        my: 2,
+        mx: 'auto',
+        p: 2,
+      }}
+    >
+      <Stack spacing={1} direction="row" alignItems="center">
+        <Avatar><TramIcon/></Avatar>
+        <Typography noWrap >{nameTrain}</Typography>
+      </Stack>
+    </Item>
+  </Box>
+  )
+}
--- src/components/directionMap.js
+++ src/components/directionMap.js
@@ -1,16 +1,47 @@
 import React from 'react'
 import { TextField, Button } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
+import SwapVerticalCircleIcon from '@mui/icons-material/SwapVerticalCircle';
+import './css/directionMap.css'
+
+import styled from 'styled-components';
 
 export default function DirectionMap() {
     const TextFieldStyled = styled(TextField)`
-    width: 68%;
+    width: 80%;
     `
+    
     const ButtonSearchStyled = styled(Button)`
-    width: 30%;
+    width: 40%;
     `
+   
   return (
     <div>
-      <div className='search-containe<div className='search-container'>'></div>
+        <div className='search-container' >
+            <div className="direction-container">
+                    <TextFieldStyled name = 'search-info' 
+                        id="outlined-basic" 
+                        margin="dense"
+                        label="Điểm xuất phát"  
+                        size="small" ></TextFieldStyled>  
+                    <TextFieldStyled name = 'search-info' 
+                        id="outlined-basic" 
+                        margin="dense"
+                        label="Điểm kết thúc"  
+                        size="small" ></TextFieldStyled>
+                    <div className='button'>      
+                             
+                    <ButtonSearchStyled size="small" 
+                    variant="contained" 
+                    >Bắt đầu</ButtonSearchStyled>
+                    <ButtonSearchStyled size="small" 
+                    variant="contained" 
+                    ><SwapVerticalCircleIcon/></ButtonSearchStyled>
+                    </div>
+                    
+            </div>
+        </div>
+
     </div>
   )
 }
