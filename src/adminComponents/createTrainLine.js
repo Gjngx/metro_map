add0ca776761cd98e99021a359cdd392b1a35e0a
phuong226 <thanhphuongtp2002@gmail.com>
2023-11-15 17:14:07 +0700

    P1

--- src/adminComponents/createTrainLine.js
+++ src/adminComponents/createTrainLine.js
@@ -15,20 +15,79 @@ function CreateTrainLineComponent() {
     trangThai: '',
   });
 
+  const [errors, setErrors] = useState({
+    soTuyenTau: '',
+    tenTuyenTau: '',
+    doDaiTuyenTau: '',
+    thoiGianBatDau: '',
+    thoiGianKetThuc: '',
+    giaVe: '',
+    khuVuc: '',
+    moTa: '',   
+  });
+
   const navigate = useNavigate();
 
   const saveTrainLine = (e) => {
     e.preventDefault();
-
-    TrainLineService.createTrainLine(newTrainLine)
-      .then((response) => {
-        console.log(response.data);
-        navigate('/admin/Tuyến tàu');
-      })
-      .catch((error) => {
-        handleError(error);
-      });
+    if (validateForm()) {
+      TrainLineService.createTrainLine(newTrainLine)
+        .then((response) => {
+          console.log(response.data);
+          navigate('/admin/Tuyến tàu');
+        })
+        .catch((error) => {
+          handleError(error);
+        });
+    }
   };
+
+  const validateForm = () => {
+    let valid = true;
+    const newErrors = { ...errors };
+    if (!newTrainLine.soTuyenTau) {
+        newErrors.soTuyenTau = 'Vui lòng nhập lại số tuyến tàu';
+        valid = false;
+      }
+    
+      if (!newTrainLine.tenTuyenTau) {
+        newErrors.tenTuyenTau = 'Vui lòng nhập lại tên tuyến tàu';
+        valid = false;
+      }
+
+      if (!newTrainLine.doDaiTuyenTau) {
+        newErrors.doDaiTuyenTau = 'Vui lòng nhập lại độ dài tuyến tàu';
+        valid = false;
+      }
+  
+      if (!newTrainLine.thoiGianBatDau) {
+        newErrors.thoiGianBatDau = 'Vui lòng nhập lại thời gian bắt đầu';
+        valid = false;
+      }
+  
+      if (!newTrainLine.thoiGianKetThuc) {
+        newErrors.thoiGianKetThuc = 'Vui lòng nhập lại thời gian kết thúc';
+        valid = false;
+      }
+  
+      if (!newTrainLine.giaVe) {
+        newErrors.giaVe = 'Vui lòng nhập lại giá vé';
+        valid = false;
+      }
+  
+      if (!newTrainLine.khuVuc) {
+        newErrors.khuVuc = 'Vui lòng nhập lại khu vực';
+        valid = false;
+      }
+
+      if (!newTrainLine.moTa) {
+        newErrors.moTa = 'Vui lòng nhập lại mô tả';
+        valid = false;
+      }
+      setErrors(newErrors);
+      return valid;
+    };
+
 
   const handleError = (error) => {
     alert('Thêm mới thất bại!');
@@ -51,8 +110,12 @@ function CreateTrainLineComponent() {
                     type="text"
                     name="soTuyenTau"
                     value={newTrainLine.soTuyenTau}
-                    onChange={(e) => setNewTrainLine({ ...newTrainLine, soTuyenTau: e.target.value })}
+                    onChange={(e) => {
+                      setNewTrainLine({ ...newTrainLine, soTuyenTau: e.target.value });
+                      setErrors({ ...errors, soTuyenTau: '' });
+                    }}
                   />
+                  {errors.soTuyenTau && <p style={{ color: 'red' }}>{errors.soTuyenTau}</p>}
                 </div>
 
                 <div className="form-group mb-2">
@@ -61,8 +124,12 @@ function CreateTrainLineComponent() {
                     type="text"
                     name="tenTuyenTau"
                     value={newTrainLine.tenTuyenTau}
-                    onChange={(e) => setNewTrainLine({ ...newTrainLine, tenTuyenTau: e.target.value })}
+                    onChange={(e) => {
+                      setNewTrainLine({ ...newTrainLine, tenTuyenTau: e.target.value });
+                      setErrors({ ...errors, tenTuyenTau: '' });
+                    }}
                   />
+                  {errors.soTuyenTau && <p style={{ color: 'red' }}>{errors.soTuyenTau}</p>}
                 </div>
 
                 <div className="form-group mb-2">
@@ -72,8 +139,12 @@ function CreateTrainLineComponent() {
                     step="any"
                     name="doDaiTuyenTau"
                     value={newTrainLine.doDaiTuyenTau}
-                    onChange={(e) => setNewTrainLine({ ...newTrainLine, doDaiTuyenTau: e.target.value })}
+                    onChange={(e) => {
+                      setNewTrainLine({ ...newTrainLine, doDaiTuyenTau: e.target.value });
+                      setErrors({ ...errors, doDaiTuyenTau: '' });
+                    }}
                   />
+                  {errors.doDaiTuyenTau && <p style={{ color: 'red' }}>{errors.doDaiTuyenTau}</p>}
                 </div>
 
                 <div className="form-group mb-2">
@@ -82,8 +153,12 @@ function CreateTrainLineComponent() {
                     type="text"
                     name="thoiGianBatDau"
                     value={newTrainLine.thoiGianBatDau}
-                    onChange={(e) => setNewTrainLine({ ...newTrainLine, thoiGianBatDau: e.target.value })}
-                      />
+                    onChange={(e) => {
+                      setNewTrainLine({ ...newTrainLine, thoiGianBatDau: e.target.value });
+                      setErrors({ ...errors, thoiGianBatDau: '' });
+                    }}
+                  />
+                  {errors.thoiGianBatDau && <p style={{ color: 'red' }}>{errors.thoiGianBatDau}</p>}
                 </div>
 
                 <div className="form-group mb-2">
@@ -92,17 +167,26 @@ function CreateTrainLineComponent() {
                     type="text"
                     name="thoiGianKetThuc"
                     value={newTrainLine.thoiGianKetThuc}
-                    onChange={(e) => setNewTrainLine({ ...newTrainLine, thoiGianKetThuc: e.target.value })}
+                    onChange={(e) => {
+                      setNewTrainLine({ ...newTrainLine, thoiGianKetThuc: e.target.value });
+                      setErrors({ ...errors, thoiGianKetThuc: '' });
+                    }}
                   />
+                  {errors.thoiGianKetThuc && <p style={{ color: 'red' }}>{errors.thoiGianKetThuc}</p>}
                 </div>
+
                 <div className="form-group mb-2">
                   <label className="form-label">Giá vé: </label>
                   <input
                     type="number"
                     name="giaVe"
                     value={newTrainLine.giaVe}
-                    onChange={(e) => setNewTrainLine({ ...newTrainLine, giaVe: e.target.value })}
+                    onChange={(e) => {
+                      setNewTrainLine({ ...newTrainLine, giaVe: e.target.value });
+                      setErrors({ ...errors, giaVe: '' });
+                    }}
                   />
+                  {errors.giaVe && <p style={{ color: 'red' }}>{errors.giaVe}</p>}
                 </div>
 
                 <div className="form-group mb-2">
@@ -111,8 +195,12 @@ function CreateTrainLineComponent() {
                     type="text"
                     name="khuVuc"
                     value={newTrainLine.khuVuc}
-                    onChange={(e) => setNewTrainLine({ ...newTrainLine, khuVuc: e.target.value })}
+                    onChange={(e) => {
+                      setNewTrainLine({ ...newTrainLine, khuVuc: e.target.value });
+                      setErrors({ ...errors, khuVuc: '' });
+                    }}
                   />
+                  {errors.khuVuc && <p style={{ color: 'red' }}>{errors.khuVuc}</p>}
                 </div>
 
                 <div className="form-group mb-2">
@@ -121,8 +209,12 @@ function CreateTrainLineComponent() {
                     type="text"
                     name="moTa"
                     value={newTrainLine.moTa}
-                    onChange={(e) => setNewTrainLine({ ...newTrainLine, moTa: e.target.value })}
+                    onChange={(e) => {
+                      setNewTrainLine({ ...newTrainLine, moTa: e.target.value });
+                      setErrors({ ...errors, moTa: '' });
+                    }}
                   />
+                  {errors.moTa && <p style={{ color: 'red' }}>{errors.moTa}</p>}
                 </div>
 
                 <div className="form-group mb-2">
